03330f65ffe72db9712512db350eebca97070122
asahmed93 <56940751+asahmed93@users.noreply.github.com>
2019-12-21 10:34:42 -0500

    Characters with styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import Header from "./components/Header.js";
+import {Route, Switch} from "react-router-dom"
+import SearchForm from "./components/SearchForm";
+import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+        <Switch>
+        <Route path="/characters">
+          <CharacterList />
+        </Route>
+        <Route path="/search">
+          <SearchForm />
+        </Route>
+        <Route path="/">
+          <WelcomePage />
+        </Route>
+      </Switch>
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,30 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+function CharacterCard(props) {
+
+  const ImgWrapper = styled.img`
+    display: flex;
+    justify-content: center;
+    margin: 2% 35%;
+  `
+
+  const InfoCenter = styled.div`
+    text-align: center;
+  `
+
+  const Paras = styled.p`
+    font-size: 1.3rem;
+    color: red;
+  `
+
+  return (
+    <InfoCenter>
+      <ImgWrapper src={props.char.image} alt="Rick n Morty character"/>
+      <h2>Name: {props.char.name}</h2>
+      <Paras>Status: {props.char.status}</Paras>
+      <Paras>Species: {props.char.species}</Paras>
+    </InfoCenter>
+  );
 }
+export default CharacterCard
