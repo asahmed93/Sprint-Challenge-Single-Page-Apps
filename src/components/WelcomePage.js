2041621a244286aa3b3e60421fcb3888c85dc726
asahmed93 <56940751+asahmed93@users.noreply.github.com>
2019-12-21 10:38:49 -0500

    Added Nav Links

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,11 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+
 
 export default function WelcomePage() {
+
+  const Title = styled.h1`
+    color: blue;
+  `
+
+  const ImgWrapper = styled.img`
+    display: flex;
+    justify-content: center;
+    margin: 2% 35%;
+  `
+
+
+  
   return (
     <section className="welcome-page">
       <header>
-        <h1>Welcome to the ultimate fan site!</h1>
-        <img
+        <Title>Welcome to the ultimate fan site!</Title>
+        <ImgWrapper
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
